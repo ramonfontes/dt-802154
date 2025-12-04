d4c31a5d8086e6d9a0ab75f696c9deb6e6a920a6
Ramon Fontes <ramonreisfontes@gmail.com>
2025-12-04 10:18:15 -0300

    update repo

--- real-to-virtual-data-replay/send.py
+++ real-to-virtual-data-replay/send.py
@@ -9,7 +9,7 @@ NUM_PACKETS = 60
 sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
 addr = (f"fe80::10", DST_PORT, 0, socket.if_nametoindex(IFACE))
 
-for i in range(NUM_PACKETS+1):
+for i in range(NUM_PACKETS):
     data = os.urandom(8)
     print(f"[SEND {i+1}/60] {len(data)} bytes:", data.hex())
     sock.sendto(data, addr)

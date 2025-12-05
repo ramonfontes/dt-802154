9474ecbb9581d9f120af04260317ec0b5d63451c
Ramon Fontes <ramonreisfontes@gmail.com>
2025-12-05 07:58:07 -0300

    update code

--- reactive-digital-twin-feedback/graph.py
+++ reactive-digital-twin-feedback/graph.py
@@ -2,6 +2,26 @@ from scapy.all import *
 import matplotlib.pyplot as plt
 
 
+def fill_missing_with_zero(times, counts):
+    if not times:
+        return [], []
+
+    t_min = min(times)
+    t_max = max(times)
+
+    # Cria série contínua
+    full_times = list(range(t_min, t_max + 1))
+    full_counts = []
+
+    # Prepara dicionário para lookup
+    d = dict(zip(times, counts))
+
+    for t in full_times:
+        full_counts.append(d.get(t, 0))
+
+    return full_times, full_counts
+
+
 def pps_from_pcap_no_tcp(pcap_path):
     """
     Compute packets-per-second (PPS) while ignoring TCP packets.
@@ -39,6 +59,9 @@ pcap2 = "phy.pcap"
 
 t1, c1 = pps_from_pcap_no_tcp(pcap1)
 t2, c2 = pps_from_pcap_no_tcp(pcap2)
+
+t1, c1 = fill_missing_with_zero(t1, c1)
+t2, c2 = fill_missing_with_zero(t2, c2)
 
 # ============
 # Plot
